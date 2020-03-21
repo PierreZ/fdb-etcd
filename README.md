cac2924940d2fcf4e713c21e130486c49d180a47
Pierre Zemb <contact@pierrezemb.fr>
2020-03-21 13:17:09 +0100

    chore: bootstrap project using Vertx starter

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# fdb-etcd
- ETCD layer on top of FoundationDB
+# fdb-etcd ![https://img.shields.io/badge/vert.x-3.8.5-purple.svg](https://img.shields.io/badge/vert.x-3.8.5-purple.svg)
+
+An experiment to provide ETCD layer on top of FoundationDB
+
+## Building
+
+To launch your tests:
+```
+./gradlew clean test
+```
+
+To package your application:
+```
+./gradlew clean assemble
+```
+
+To run your application:
+```
+./gradlew clean run
+```
---
+++ src/test/java/fr/pierrezemb/fdb/layer/etcd/TestMainVerticle.java
@@ -0,0 +1,26 @@
+package fr.pierrezemb.fdb.layer.etcd;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(VertxExtension.class)
+public class TestMainVerticle {
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    testContext.completeNow();
+  }
+}
